02ee4a3358a83814deb951c4ca3d9e285f0afa9e
Noc2 <davidhawig@gmail.com>
2026-03-15 20:10:43 +0100

    Update landing page CTA

--- packages/nextjs/components/home/LandingPageActions.tsx
+++ packages/nextjs/components/home/LandingPageActions.tsx
@@ -3,13 +3,11 @@
 import { useEffect, useRef } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useConnectModal } from "@rainbow-me/rainbowkit";
 import { useAccount } from "wagmi";
 import { useScaffoldReadContract } from "~~/hooks/scaffold-eth";
 
 export function LandingPageActions() {
   const { address, isConnected } = useAccount();
-  const { openConnectModal } = useConnectModal();
   const router = useRouter();
   const wasConnected = useRef(isConnected);
 
@@ -31,16 +29,13 @@ export function LandingPageActions() {
 
   return (
     <div className="mt-7 flex flex-wrap justify-center gap-3 lg:justify-start">
-      {!isConnected && (
-        <button
-          className="btn btn-primary rounded-full px-5 text-black"
-          onClick={openConnectModal}
-          type="button"
-          style={{ fontSize: "16px" }}
-        >
-          Connect Wallet
-        </button>
-      )}
+      <Link
+        href="/vote"
+        className="btn btn-primary rounded-full px-5 text-black"
+        style={{ fontSize: "16px" }}
+      >
+        Discover Ratings
+      </Link>
       <Link
         href="/docs"
         className="btn rounded-full border border-white/10 bg-white/[0.04] text-white hover:bg-white/[0.08]"
